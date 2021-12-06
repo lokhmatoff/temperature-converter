197f499e854946a64a8755cf50848ec40c0b2a10
Dmitry Andreev <shaggydmitrydev@gmail.com>
2021-12-06 15:22:40 +0300

    Implemented converter and its CLI, updated README

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# temperature-converter
-🌡️ Convert Fahrenheits to Celsiums, Celsiums to Kelvins and so on 🌡️ | Implemented while learning Rust 🦀
+# Rusty Temperature Converter
+<small>This is the pet project</small>
+
+Convert Fahrenheit to Celsius and vice versa 🌡️ <br />
+The program was implemented in the course of studying basic concepts of Rust 🦀
+
+## Usage
+```sh
+$ temperature-converter <input_temperature> [-f]
+```
+
+- `-f` — Specify `<input_temperature>` in Fahrenheits.
+
+Input temperature can be either `int` or `float`.
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,46 @@
-const CELCIUM_KELVIN_DIFFERENCE: f32 = 273.15;
+use std::env;
 
 fn main() {
-    println!("temperature-converter");
+    let fahrenheits: String = "-f".to_string();
+    let args: Vec<String> = env::args().collect();
+
+    let input_temperature: f64 = args[1].parse().unwrap();
+    
+    let input_degrees: String = {
+        if args.contains(&fahrenheits) {
+            String::from("F")
+        } else {
+            String::from("C")
+        }
+    };
+    let output_degrees: String = {
+        if args.contains(&fahrenheits) {
+            String::from("C")
+        } else {
+            String::from("F")
+        }
+    };
+    let result: f64 = {
+        if args.contains(&fahrenheits) {
+            convert_temperature(&Temperature::F(input_temperature))
+        } else {
+            convert_temperature(&Temperature::C(input_temperature))
+        }
+    };
+
+    println!("{}", result);
+    println!("{}{} = {}{}", input_temperature, input_degrees, result, output_degrees);
 }
 
-fn celciums_to_kelvins(amount: f32) -> f32 { amount - CELCIUM_KELVIN_DIFFERENCE }
+enum Temperature {
+    C(f64),
+    F(f64),
+}
 
-fn kelvins_to_celciums(amount: f32) -> f32 { amount + CELCIUM_KELVIN_DIFFERENCE }
+
+fn convert_temperature(input_temp: &Temperature) -> f64 {
+    match input_temp {
+        &Temperature::C(degrees) => (degrees * 1.8) + 32.0,
+        &Temperature::F(degrees) => (degrees - 32.0) / 1.8,
+    }
+}
