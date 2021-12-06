c0ddb0b6bf57140281bbe517b74fa8fedf6faed6
Dmitry Andreev <shaggydmitrydev@gmail.com>
2021-12-06 12:47:06 +0300

    Implemented C -> K and K -> C convert

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+const CELCIUM_KELVIN_DIFFERENCE: f32 = 273.15;
+
 fn main() {
     println!("temperature-converter");
 }
+
+fn celciums_to_kelvins(amount: f32) -> f32 { amount - CELCIUM_KELVIN_DIFFERENCE }
+
+fn kelvins_to_celciums(amount: f32) -> f32 { amount + CELCIUM_KELVIN_DIFFERENCE }
